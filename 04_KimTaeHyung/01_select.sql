40ed3e77c36a52164fc80d8f39e355a22e5dbaa6
ikth-kim <ikth.kim@gmail.com>
2025-11-03 14:20:20 +0900

    swcamp 계정생선, DB_SCRIPT, 01_SELECT 진행중

---
+++ 04_KimTaeHyung/01_select.sql
@@ -0,0 +1,36 @@
+-- 01. SELECT : 특정 테이블의 원하는 데이터(행)을 조회하는 구문
+
+-- tbl_menu 테이블의 모든 메뉴 이름(menu_name) 조회
+
+/*범위 주석 (단축기 ctrl + shift + /) */
+/*2*/
+SELECT
+    menu_name
+/*1*/
+FROM
+    tbl_menu;
+
+-- 모든 메뉴의 menu_code, menu_name, menu_price 조회
+
+SELECT
+    menu_code,
+    menu_name,
+    menu_price
+FROM
+    tbl_menu;
+
+-- tbl_menu 테이블 모든 열 조회
+
+SELECT
+    menu_code,
+    menu_name,
+    category_code,
+    orderable_status
+FROM
+    tbl_menu;
+
+-- *(asterrisk) == 모든
+SELECT
+    *
+FROM
+    tbl_menu;
